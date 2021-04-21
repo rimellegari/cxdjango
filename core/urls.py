5de35c3a54c0ebb3a5d47ec524dbaca705ebbad9
Ricardo <rimellegari@gmail.com>
2021-04-20 23:11:39 -0300

    login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('home', home, name='home'),
